1e3bb0edc3a8c574056619e81bd2f513b44860f8
Hoang-Bao Le <72609761+baohl00@users.noreply.github.com>
2024-10-15 20:14:52 +0100

    Update inference.sh
    Update script

--- scripts/inference.sh
+++ scripts/inference.sh
@@ -1,7 +1,7 @@
-DATASET=circo # fiq-shirt, fiq-dress, fiq-toptee, circo
+DATASET=circo # fiq-shirt, fiq-dress, fiq-toptee, circo, cirr 
 SIZE=large
 k=51
-NOTE=gmlp_concat16_avg # attn features = dim//4
+NOTE=resmlp # vision projector type 
 
 echo "-- INFERENCE of DATA: ${DATASET} --"
 
@@ -10,8 +10,6 @@ python inference.py \
 	--model_path ./models/magic_lens_clip_$SIZE.pkl \
 	--dataset $DATASET \
 	--top_k $k \
-	--note $NOTE # iat = img and txt
+	--note $NOTE 
 
 echo ">>> Evaluation: dataset $DATASET , size ${SIZE}, top_${k}, note ${NOTE}"
-#python3 eval.py \
-#	--predict ./output/circo_${SIZE}_${k}gmlp/circo_results.json  
